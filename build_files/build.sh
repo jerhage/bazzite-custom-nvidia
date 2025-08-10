3f0b242c37efb97bbf749d3077763be5f2493857
Jeremy Hage <jhage.codingkr@gmail.com>
2025-08-10 16:06:13 -0500

    add /opt dir for 1password install

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,8 +11,8 @@ enable_copr() {
 }
 
 # https://support.1password.com/install-linux/#fedora-or-red-hat-enterprise-linux
+mkdir /opt/1Password
 get_1_password() {
-    mkdir -p /opt/1Password
     rpm --import https://downloads.1password.com/linux/keys/1password.asc
     sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
     dnf install -y 1password
